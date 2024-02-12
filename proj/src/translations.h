c3ab6bbfc8a1b988d63874cf8e431a8c44c51c41
filipton <filipton12@gmail.com>
2024-02-12 18:37:44 +0100

    feat: dynamic translations (shitshow)

--- proj/src/translations.h
+++ proj/src/translations.h
@@ -1,38 +1,25 @@
 #ifndef __TRANSLATIONS_H__
 #define __TRANSLATIONS_H__
 
-// EN
+#include "globals.hpp"
+
+// EN / PL
 #define TR_STACKMAT_HEADER "Timer"
 #define TR_SERVER_HEADER "Server"
-#define TR_DISCONNECTED "Disconnected"
-#define TR_SOLVER "Competitor"
-#define TR_CONFIRM_TIME "Confirm the time"
-#define TR_AWAITING_JUDGE "Scan the judge's card"
+#define TR_DISCONNECTED (primaryLangauge ? "Disconnected" : "Rozlaczony")
+#define TR_SOLVER (primaryLangauge ? "Competitor" : "Zawodnik")
+#define TR_CONFIRM_TIME (primaryLangauge ? "Confirm the time" : "Potwierdz czas")
+#define TR_AWAITING_JUDGE (primaryLangauge ? "Scan the judge's card" : "Zeskanuj karte sedziego")
 #define TR_UNHANDLED_STATE "Unhandled state!"
 
-#define TR_AWAITING_SOLVER_TOP "Scan the card"
-#define TR_AWAITING_SOLVER_BOTTOM "of a competitor"
+#define TR_AWAITING_SOLVER_TOP (primaryLangauge ? "Scan the card" : "Zeskanuj karte")
+#define TR_AWAITING_SOLVER_BOTTOM (primaryLangauge ? "of a competitor" : "zawodnika")
 
-#define TR_DELEGATE_HEADER "Delegate"
-#define TR_DELEGATE_COUNTDOWN "In %lu seconds!"
-#define TR_DELEGATE_CALLED_TOP "Delegate called"
-#define TR_DELEGATE_CALLED_BOTTOM "Release button"
+#define TR_DELEGATE_HEADER (primaryLangauge ? "Delegate" : "Delegat")
+#define TR_DELEGATE_COUNTDOWN (primaryLangauge ? "In %lu seconds!" : "Za %lu sekund!")
+#define TR_DELEGATE_CALLED_TOP (primaryLangauge ? "Delegate called" : "Delegat wezwany")
+#define TR_DELEGATE_CALLED_BOTTOM (primaryLangauge ? "Release button" : "Pusc przycisk")
 
-// PL
-// #define TR_STACKMAT_HEADER "Timer"
-// #define TR_SERVER_HEADER "Server"
-// #define TR_DISCONNECTED "Rozlaczony"
-// #define TR_SOLVER "Zawodnik"
-// #define TR_CONFIRM_TIME "Potwierdz czas"
-// #define TR_AWAITING_JUDGE "Zeskanuj karte sedziego"
-// #define TR_UNHANDLED_STATE "Stan bez obsługi!"
-
-// #define TR_AWAITING_SOLVER_TOP "Zeskanuj karte"
-// #define TR_AWAITING_SOLVER_BOTTOM "zawodnika"
-
-// #define TR_DELEGATE_HEADER "Delegat"
-// #define TR_DELEGATE_COUNTDOWN "Za %lu sekund!"
-// #define TR_DELEGATE_CALLED_TOP "Delegat wezwany"
-// #define TR_DELEGATE_CALLED_BOTTOM "Pusc przycisk"
+#define TR_SOLVE_ENTRY_HEADER "Error"
 
 #endif
