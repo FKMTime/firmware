8d5ff95f8e329566f308f8af0f7055bb09c2ebaa
filipton <filipton12@gmail.com>
2024-03-06 22:48:06 +0100

    chore: simple release script (for stable releases)

---
+++ release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+if ! command -v gh &> /dev/null
+then
+    echo "'gh' could not be found"
+    exit
+fi
+
+if ! command -v pio &> /dev/null
+then
+    echo "'pio' could not be found"
+    exit
+fi
+
+cd $SCRIPT_DIR/proj
+pio run
+
+cd $SCRIPT_DIR
+VERSION=$(head -c 8 ./proj/.versum)
+BUILD_TIME=$(printf "%d\n" "0x$(cat ./proj/src/version.h | grep 'BUILD_TIME' | cut -d'"' -f 2)")
+BUILD_TIME_HEX=$(printf "%08x\n" $BUILD_TIME)
+
+if gh release list | grep -q "$BUILD_TIME-$VERSION" ; then
+    echo "Release already exists"
+    exit
+fi
+
+BUILD_FILES=$(ls $SCRIPT_DIR/proj/build/*.$VERSION.$BUILD_TIME_HEX.bin)
+if [ -z "$BUILD_FILES" ]; then
+    echo "No build files found"
+    exit
+fi
+
+gh release create "$BUILD_TIME-$VERSION" -t "$BUILD_TIME-$VERSION" --generate-notes
+for file in $BUILD_FILES; do
+    echo "Uploading $file"
+    gh release upload "$BUILD_TIME-$VERSION" "$file"
+done
