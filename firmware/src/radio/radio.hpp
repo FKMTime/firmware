cf2f0a5aee3a6072369dc829d78ad2b30dc95beb
filipton <filipton12@gmail.com>
2024-04-03 20:17:35 +0200

    feat: non blocking wifimanager

--- firmware/src/radio/radio.hpp
+++ firmware/src/radio/radio.hpp
@@ -13,23 +13,35 @@
 void apCallback(WiFiManager *wm);
 
 void initWifi() {
+  WiFi.mode(WIFI_STA); 
   WiFiManager wm;
 
   char generatedDeviceName[100];
   snprintf(generatedDeviceName, 100, "%s-%x", NAME_PREFIX, getEspId());
 
   wm.setConfigPortalTimeout(300);
-  wm.setAPCallback(apCallback);
-  // wm.setConfigPortalBlocking(false);
+  wm.setConfigPortalBlocking(false);
+  // wm.setAPCallback(apCallback);
 
   bool res = wm.autoConnect(generatedDeviceName, WIFI_PASSWORD);
-  if (!res) {
-    Logger.println("Failed to connect to wifi... Restarting!");
-    delay(1500);
-    ESP.restart();
+  if (res) {
+    Logger.printf("Connected!");
+  } else {
+    initBt(generatedDeviceName);
+    Logger.printf("Config portal running!");
   }
 
-  deinitBt();
+  while(!res && !wm.process()) {
+    delay(5);
+  }
+
+  // if (!res) {
+  //   Logger.println("Failed to connect to wifi... Restarting!");
+  //   delay(1500);
+  //   ESP.restart();
+  // }
+
+  if (!res) deinitBt();
   configTime(3600, 0, "pool.ntp.org", "time.nist.gov", "time.google.com");
   initWs();
 }
