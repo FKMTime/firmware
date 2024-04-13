3e767e900d63bb7e4f64f485c084523dbf4ec8f6
filipton <filipton12@gmail.com>
2024-04-13 10:34:59 +0200

    fix: stop Inspection function

--- firmware/src/state.hpp
+++ firmware/src/state.hpp
@@ -1,11 +1,11 @@
 #ifndef __STATE_HPP__
 #define __STATE_HPP__
 
-#include <stackmat.h>
-#include <UUID.h>
-#include "ws_logger.h"
-#include "translations.h"
 #include "lcd.hpp"
+#include "translations.h"
+#include "ws_logger.h"
+#include <UUID.h>
+#include <stackmat.h>
 
 #define UUID_LENGTH 37
 String displayTime(uint8_t m, uint8_t s, uint16_t ms);
@@ -21,51 +21,51 @@ bool lastServerConnected = false;
 bool lastStackmatConnected = false;
 
 enum StateScene {
-  SCENE_NOT_INITALIZED, // before timer connects to wifi/ws
+  SCENE_NOT_INITALIZED,         // before timer connects to wifi/ws
   SCENE_WAITING_FOR_COMPETITOR, // before competitor scans card
-  SCENE_COMPETITOR_INFO, // competitor info with inspection info
+  SCENE_COMPETITOR_INFO,        // competitor info with inspection info
 
   // FROM HERE, DO NOT SHOW TIMER/SERVER DISCONNECTED
-  SCENE_INSPECTION, // during inspection (show inspection time etc)
-  SCENE_TIMER_TIME, // during solve
+  SCENE_INSPECTION,    // during inspection (show inspection time etc)
+  SCENE_TIMER_TIME,    // during solve
   SCENE_FINISHED_TIME, // after solve
-  SCENE_ERROR // after error
+  SCENE_ERROR          // after error
 };
 
 struct State {
-    StateScene currentScene = SCENE_NOT_INITALIZED;
+  StateScene currentScene = SCENE_NOT_INITALIZED;
 
-    char solveSessionId[UUID_LENGTH];
-    int solveTime = 0;
-    int lastSolveTime = 0;
-    int penalty = 0;
+  char solveSessionId[UUID_LENGTH];
+  int solveTime = 0;
+  int lastSolveTime = 0;
+  int penalty = 0;
 
-    bool added = true;
-    bool useInspection = true;
-    unsigned long inspectionStarted = 0;
-    unsigned long inspectionEnded = 0;
+  bool added = true;
+  bool useInspection = true;
+  unsigned long inspectionStarted = 0;
+  unsigned long inspectionEnded = 0;
 
-    unsigned long competitorCardId = 0;
-    unsigned long judgeCardId = 0;
-    char competitorDisplay[128]; // max 128 chars
+  unsigned long competitorCardId = 0;
+  unsigned long judgeCardId = 0;
+  char competitorDisplay[128]; // max 128 chars
 
-    bool timeConfirmed = false;
-    bool waitingForSolveResponse = false;
+  bool timeConfirmed = false;
+  bool waitingForSolveResponse = false;
 
-    StackmatTimerState lastTimerState = ST_Unknown;
+  StackmatTimerState lastTimerState = ST_Unknown;
 
-    char errorMsg[128]; // max 128 chars
-    StateScene sceneBeforeError = SCENE_NOT_INITALIZED;
+  char errorMsg[128]; // max 128 chars
+  StateScene sceneBeforeError = SCENE_NOT_INITALIZED;
 } state;
 
 // TODO: save state to EEPROM
 struct EEPROMState {
-    char solveSessionId[UUID_LENGTH];
-    unsigned long competitorCardId;
-    unsigned long inspectionStarted;
-    unsigned long inspectionEnded;
-    int solveTime;
-    int penalty;
+  char solveSessionId[UUID_LENGTH];
+  unsigned long competitorCardId;
+  unsigned long inspectionStarted;
+  unsigned long inspectionEnded;
+  int solveTime;
+  int penalty;
 };
 
 void stateDefault() {
@@ -145,151 +145,159 @@ void checkConnectionStatus() {
 }
 
 void stateLoop() {
-    checkConnectionStatus();
-    if(!stateHasChanged || lockStateChange) return;
+  checkConnectionStatus();
+  if (!stateHasChanged || lockStateChange)
+    return;
 
-    if (!state.added && WiFi.isConnected() && webSocket.isConnected()) {
-      lcdPrintf(0, true, ALIGN_CENTER, TR_DEVICE_NOT_ADDED_TOP);
-      lcdPrintf(1, true, ALIGN_CENTER, TR_DEVICE_NOT_ADDED_BOTTOM);
+  if (!state.added && WiFi.isConnected() && webSocket.isConnected()) {
+    lcdPrintf(0, true, ALIGN_CENTER, TR_DEVICE_NOT_ADDED_TOP);
+    lcdPrintf(1, true, ALIGN_CENTER, TR_DEVICE_NOT_ADDED_BOTTOM);
+    stateHasChanged = false;
+    return;
+  }
+
+  if (state.currentScene <= SCENE_WAITING_FOR_COMPETITOR) {
+    if (!WiFi.isConnected()) {
+      lcdPrintf(0, true, ALIGN_CENTER, TR_WIFI_HEADER);
+      lcdPrintf(1, true, ALIGN_CENTER, TR_DISCONNECTED);
+      stateHasChanged = false;
+      return;
+    } else if (!webSocket.isConnected()) {
+      lcdPrintf(0, true, ALIGN_CENTER, TR_SERVER_HEADER);
+      lcdPrintf(1, true, ALIGN_CENTER, TR_DISCONNECTED);
+      stateHasChanged = false;
+      return;
+    } else if (!stackmat.connected()) {
+      lcdPrintf(0, true, ALIGN_CENTER, TR_STACKMAT_HEADER);
+      lcdPrintf(1, true, ALIGN_CENTER, TR_DISCONNECTED);
+      stateHasChanged = false;
+      return;
+    }
+  }
+
+  if (state.currentScene == SCENE_WAITING_FOR_COMPETITOR) {
+    lcdPrintf(0, true, ALIGN_CENTER, TR_AWAITING_COMPETITOR_TOP);
+    lcdPrintf(1, true, ALIGN_CENTER, TR_AWAITING_COMPETITOR_BOTTOM);
+  } else if (state.currentScene == SCENE_COMPETITOR_INFO) {
+    lcdPrintf(0, true, ALIGN_CENTER, state.competitorDisplay);
+    lcdPrintf(1, true, ALIGN_CENTER, state.useInspection ? "Inspection" : "");
+  } else if (state.currentScene == SCENE_TIMER_TIME) {
+    // lcdPrintf(0, true, ALIGN_CENTER, "%s",
+    // displayTime(stackmat.displayMinutes(), stackmat.displaySeconds(),
+    // stackmat.displayMilliseconds()).c_str()); lcdClearLine(1);
+  } else if (state.currentScene == SCENE_INSPECTION) {
+    int time = millis() - state.inspectionStarted;
+    int secondsLeft = (int)ceil((time) / 1000);
+    uint16_t ms = (time) % 1000;
+    lcdPrintf(0, true, ALIGN_CENTER, "%d.%03d s", secondsLeft, ms);
+    lcdClearLine(1);
+    delay(5);
+    stateHasChanged = true; // refresh
+    return;
+  } else if (state.currentScene == SCENE_FINISHED_TIME) {
+    if (waitForSolveResponse) {
+      lcdPrintf(0, true, ALIGN_CENTER, TR_WAITING_FOR_SOLVE_TOP);
+      lcdPrintf(1, true, ALIGN_CENTER, TR_WAITING_FOR_SOLVE_BOTTOM);
+
       stateHasChanged = false;
       return;
     }
 
-    if (state.currentScene <= SCENE_WAITING_FOR_COMPETITOR) {
-      if (!WiFi.isConnected()) {
-        lcdPrintf(0, true, ALIGN_CENTER, TR_WIFI_HEADER);
-        lcdPrintf(1, true, ALIGN_CENTER, TR_DISCONNECTED);
-        stateHasChanged = false;
-        return;
-      } else if (!webSocket.isConnected()) {
-        lcdPrintf(0, true, ALIGN_CENTER, TR_SERVER_HEADER);
-        lcdPrintf(1, true, ALIGN_CENTER, TR_DISCONNECTED);
-        stateHasChanged = false;
-        return;
-      } else if (!stackmat.connected()) {
-        lcdPrintf(0, true, ALIGN_CENTER, TR_STACKMAT_HEADER);
-        lcdPrintf(1, true, ALIGN_CENTER, TR_DISCONNECTED);
-        stateHasChanged = false;
-        return;
+    uint8_t minutes = state.solveTime / 60000;
+    uint8_t seconds = (state.solveTime % 60000) / 1000;
+    uint16_t ms = state.solveTime % 1000;
+
+    int inspectionTime = state.inspectionEnded - state.inspectionStarted;
+    int inspectionS = (inspectionTime % 60000) / 1000;
+
+    String solveTimeStr = displayTime(minutes, seconds, ms);
+
+    /* Line 1 */
+    if (state.solveTime > 0) {
+      if (inspectionTime >= INSPECTION_TIME) {
+        lcdPrintf(0, true, ALIGN_LEFT, "%s (%ds)", solveTimeStr.c_str(),
+                  inspectionS);
+      } else {
+        lcdPrintf(0, true, ALIGN_LEFT, "%s", solveTimeStr.c_str());
       }
+    } else {
+      lcdClearLine(0);
     }
 
-    if (state.currentScene == SCENE_WAITING_FOR_COMPETITOR) {
-        lcdPrintf(0, true, ALIGN_CENTER, TR_AWAITING_COMPETITOR_TOP);
-        lcdPrintf(1, true, ALIGN_CENTER, TR_AWAITING_COMPETITOR_BOTTOM);
-    } else if(state.currentScene == SCENE_COMPETITOR_INFO) {
-        lcdPrintf(0, true, ALIGN_CENTER, state.competitorDisplay);
-        lcdPrintf(1, true, ALIGN_CENTER, state.useInspection ? "Inspection" : "");
-    } else if(state.currentScene == SCENE_TIMER_TIME) {
-        // lcdPrintf(0, true, ALIGN_CENTER, "%s", displayTime(stackmat.displayMinutes(), stackmat.displaySeconds(), stackmat.displayMilliseconds()).c_str());
-        // lcdClearLine(1);
-    } else if(state.currentScene == SCENE_INSPECTION) {
-      int time = millis() - state.inspectionStarted;
-      int secondsLeft = (int)ceil((time) / 1000); 
-      uint16_t ms = (time) % 1000;
-      lcdPrintf(0, true, ALIGN_CENTER, "%d.%03d s", secondsLeft, ms);
-      lcdClearLine(1);
-      delay(5);
-      stateHasChanged = true; // refresh
-      return;
-    } else if (state.currentScene == SCENE_FINISHED_TIME) {
-        if (waitForSolveResponse) {
-            lcdPrintf(0, true, ALIGN_CENTER, TR_WAITING_FOR_SOLVE_TOP);
-            lcdPrintf(1, true, ALIGN_CENTER, TR_WAITING_FOR_SOLVE_BOTTOM);
-
-            stateHasChanged = false;
-            return;
-        }
-
-        uint8_t minutes = state.solveTime / 60000;
-        uint8_t seconds = (state.solveTime % 60000) / 1000;
-        uint16_t ms = state.solveTime % 1000;
-
-        int inspectionTime = state.inspectionEnded - state.inspectionStarted;
-        int inspectionS = (inspectionTime % 60000) / 1000;
-
-        String solveTimeStr = displayTime(minutes, seconds, ms);
-
-        /* Line 1 */
-        if (state.solveTime > 0) {
-          if (inspectionTime >= INSPECTION_TIME) {
-            lcdPrintf(0, true, ALIGN_LEFT, "%s (%ds)", solveTimeStr.c_str(), inspectionS);
-          } else {
-            lcdPrintf(0, true, ALIGN_LEFT, "%s", solveTimeStr.c_str());
-          }
-        } else {
-          lcdClearLine(0);
-        }
-
-        if (state.penalty == -1) {
-            lcdPrintf(0, false, ALIGN_RIGHT, "DNF");
-        } else if(state.penalty > 0) {
-            lcdPrintf(0, false, ALIGN_RIGHT, "+%d", state.penalty);
-        }
-
-        /* Line 2 */
-        if (!state.timeConfirmed) {
-            lcdPrintf(1, true, ALIGN_RIGHT, TR_CONFIRM_TIME);
-        } else if (state.judgeCardId == 0) {
-            lcdPrintf(1, true, ALIGN_RIGHT, TR_AWAITING_JUDGE);
-        } else if(state.judgeCardId > 0 && state.competitorCardId > 0) {
-            lcdPrintf(1, true, ALIGN_RIGHT, TR_AWAITING_COMPETITOR_AGAIN);
-        }
-    } else if (state.currentScene == SCENE_ERROR) {
-        lcdPrintf(0, true, ALIGN_CENTER, TR_ERROR_HEADER);
-        lcdPrintf(1, true, ALIGN_CENTER, state.errorMsg);
+    if (state.penalty == -1) {
+      lcdPrintf(0, false, ALIGN_RIGHT, "DNF");
+    } else if (state.penalty > 0) {
+      lcdPrintf(0, false, ALIGN_RIGHT, "+%d", state.penalty);
     }
 
-    stateHasChanged = false;
+    /* Line 2 */
+    if (!state.timeConfirmed) {
+      lcdPrintf(1, true, ALIGN_RIGHT, TR_CONFIRM_TIME);
+    } else if (state.judgeCardId == 0) {
+      lcdPrintf(1, true, ALIGN_RIGHT, TR_AWAITING_JUDGE);
+    } else if (state.judgeCardId > 0 && state.competitorCardId > 0) {
+      lcdPrintf(1, true, ALIGN_RIGHT, TR_AWAITING_COMPETITOR_AGAIN);
+    }
+  } else if (state.currentScene == SCENE_ERROR) {
+    lcdPrintf(0, true, ALIGN_CENTER, TR_ERROR_HEADER);
+    lcdPrintf(1, true, ALIGN_CENTER, state.errorMsg);
+  }
+
+  stateHasChanged = false;
 }
 
 /// @brief Called after time is finished
-/// @param solveTime 
+/// @param solveTime
 void startSolveSession(int solveTime) {
-    if (solveTime == state.lastSolveTime) return;
+  if (solveTime == state.lastSolveTime)
+    return;
 
-    uuid.generate(); // generate next uuid
+  uuid.generate(); // generate next uuid
 
-    strncpy(state.solveSessionId, uuid.toCharArray(), UUID_LENGTH);
-    state.solveTime = solveTime;
-    state.lastSolveTime = solveTime;
-    state.penalty = 0;
-    state.judgeCardId = 0;
-    state.timeConfirmed = false;
-    waitForSolveResponse = false;
-    state.currentScene = SCENE_FINISHED_TIME;
+  strncpy(state.solveSessionId, uuid.toCharArray(), UUID_LENGTH);
+  state.solveTime = solveTime;
+  state.lastSolveTime = solveTime;
+  state.penalty = 0;
+  state.judgeCardId = 0;
+  state.timeConfirmed = false;
+  waitForSolveResponse = false;
+  state.currentScene = SCENE_FINISHED_TIME;
 
-    int inspectionTime = state.inspectionEnded - state.inspectionStarted;
-    if (inspectionTime >= INSPECTION_PLUS_TWO_PENALTY && inspectionTime < INSPECTION_DNF_PENALTY) {
-      state.penalty = 2;
-    } else if (inspectionTime >= INSPECTION_DNF_PENALTY) {
-      state.penalty = -1;
-    }
+  int inspectionTime = state.inspectionEnded - state.inspectionStarted;
+  if (inspectionTime >= INSPECTION_PLUS_TWO_PENALTY &&
+      inspectionTime < INSPECTION_DNF_PENALTY) {
+    state.penalty = 2;
+  } else if (inspectionTime >= INSPECTION_DNF_PENALTY) {
+    state.penalty = -1;
+  }
 
-    stateHasChanged = true;
-    saveState();
+  stateHasChanged = true;
+  saveState();
 }
 
 void resetSolveState(bool save = true) {
-    state.solveTime = 0;
-    state.penalty = 0;
-    state.competitorCardId = 0;
-    state.judgeCardId = 0;
-    state.timeConfirmed = false;
-    state.inspectionStarted = 0;
-    state.inspectionEnded = 0;
-    memset(state.competitorDisplay, ' ', sizeof(state.competitorDisplay));
-    waitForSolveResponse = false;
-    state.currentScene = SCENE_WAITING_FOR_COMPETITOR;
+  state.solveTime = 0;
+  state.penalty = 0;
+  state.competitorCardId = 0;
+  state.judgeCardId = 0;
+  state.timeConfirmed = false;
+  state.inspectionStarted = 0;
+  state.inspectionEnded = 0;
+  memset(state.competitorDisplay, ' ', sizeof(state.competitorDisplay));
+  waitForSolveResponse = false;
+  state.currentScene = SCENE_WAITING_FOR_COMPETITOR;
 
-    stateHasChanged = true;
+  stateHasChanged = true;
 
-    if(save) saveState();
+  if (save)
+    saveState();
 }
 
 void startInspection() {
-  if (state.currentScene >= SCENE_INSPECTION) return;
-  if (state.competitorCardId <= 0) return;
+  if (state.currentScene >= SCENE_INSPECTION)
+    return;
+  if (state.competitorCardId <= 0)
+    return;
 
   state.currentScene = SCENE_INSPECTION;
   state.inspectionStarted = millis();
@@ -297,15 +305,20 @@ void startInspection() {
 }
 
 void stopInspection() {
-  if (state.currentScene != SCENE_INSPECTION) return;
+  if (state.inspectionStarted == 0)
+    return;
+
+  // i think this code causes errors!
+  // if (state.currentScene != SCENE_INSPECTION) return;
 
   state.currentScene = SCENE_TIMER_TIME;
   state.inspectionEnded = millis();
   stateHasChanged = true;
 }
 
-void showError(const char* str) {
-  if(state.currentScene != SCENE_ERROR) state.sceneBeforeError = state.currentScene;
+void showError(const char *str) {
+  if (state.currentScene != SCENE_ERROR)
+    state.sceneBeforeError = state.currentScene;
   state.currentScene = SCENE_ERROR;
   strncpy(state.errorMsg, str, 128);
   stateHasChanged = true;
@@ -340,7 +353,7 @@ void sendSolve(bool delegate) {
   time_t epoch;
   time(&epoch);
 
-  if(delegate) {
+  if (delegate) {
     uuid.generate();
     strncpy(state.solveSessionId, uuid.toCharArray(), UUID_LENGTH);
   }
@@ -354,18 +367,20 @@ void sendSolve(bool delegate) {
   doc["solve"]["timestamp"] = epoch;
   doc["solve"]["session_id"] = state.solveSessionId;
   doc["solve"]["delegate"] = delegate;
-  doc["solve"]["inspection_time"] = state.inspectionEnded - state.inspectionStarted;
+  doc["solve"]["inspection_time"] =
+      state.inspectionEnded - state.inspectionStarted;
 
   String json;
   serializeJson(doc, json);
   webSocket.sendTXT(json);
 
-  if(!webSocket.isConnected()) {
+  if (!webSocket.isConnected()) {
     showError("Server not connected!");
   }
 
   // only show wait for solve response if not delegate request
-  if(!delegate) waitForSolveResponse = true;
+  if (!delegate)
+    waitForSolveResponse = true;
 }
 
 void logState() {
