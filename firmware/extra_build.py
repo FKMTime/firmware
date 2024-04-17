cb99ea13755ab401cece1a18cb1f6897b10710d9
filipton <filipton12@gmail.com>
2024-04-17 11:25:14 +0200

    wip: better updates/versions (again LMAOO)

--- firmware/extra_build.py
+++ firmware/extra_build.py
@@ -1,69 +1,28 @@
 Import("env")
 import os, time, os
 
-OTA_PROJ_NAME = "fkm"
-
-def curl_get_ota_version(project, channel, chip):
-    res = os.popen(f"curl -s https://ota.filipton.space/firmware/{project}/{channel}/{chip}/latest.json").read()
-    print("OTA response:", res)
-    version = os.popen(f"echo '{res}' | jq -r .version").read().strip()
-
-    return version
-
-def curl_upload_ota(project, channel, chip, version, name, bin, token, buildTime):
-    print(f"Uploading {bin} to OTA")
-    print(f"Url: https://ota.filipton.space/latest/{project}/{channel}/{chip}/{version}/{buildTime}/{name}.bin")
-    os.popen(f"curl -s -T {bin} -H 'Authorization: {token}' https://ota.filipton.space/latest/{project}/{channel}/{chip}/{version}/{buildTime}/{name}.bin")
-
 def after_build(source, target, env):
-    release_build = "RELEASE_BUILD" in env["ENV"]
-
     version = os.popen("cat src/version.h | grep \"FIRMWARE_VERSION\" | cut -d'\"' -f 2").read().strip()
     buildTime = os.popen("cat src/version.h | grep \"BUILD_TIME\" | cut -d'\"' -f 2").read().strip()
     firmwareType = os.popen("cat src/version.h | grep \"FIRMWARE_TYPE\" | cut -d'\"' -f 2").read().strip()
     chip = env['BOARD_MCU']
-    channel = release_build == True and "stable" or "prerelease"
-    name = release_build == True and version or buildTime
 
-    if "OTA_TOKEN" in env["ENV"]:
-        curl_upload_ota(firmwareType, channel, chip, version, name, source[0].get_abspath(), env["ENV"]["OTA_TOKEN"], buildTime)
-    else:
-        os.popen(f"mkdir -p ../build ; rm -f ../build/{chip}.{firmwareType}.*.bin ; cp {source[0].get_abspath()} ../build/{chip}.{firmwareType}.{version}.bin")
-        print("OTA_TOKEN is not set! Skipping OTA upload")
+    os.popen(f"mkdir -p ../build ; rm -f ../build/{chip}.{firmwareType}.*.bin ; cp {source[0].get_abspath()} ../build/{chip}.{firmwareType}.{version}.bin")
 
 def generate_version():
     release_build = "RELEASE_BUILD" in env["ENV"]
-    filesHash = os.popen("bash ./hash.sh").read().strip()
-
-    if not release_build:
-        try:
-            with open(".versum", "r") as file:
-                if filesHash == file.read().strip():
-                    return
-        except:
-            print(".versum doesn't exists! Building...")
 
     buildTime = int(time.time())
-    version = buildTime
+    version = release_build == True and env["ENV"]["RELEASE_BUILD"] or buildTime
     versionPath = os.path.join(env["PROJECT_DIR"], "src", "version.h")
     chip = env['BOARD_MCU']
-    channel = release_build == True and "stable" or "prerelease"
-    curl_version = curl_get_ota_version(OTA_PROJ_NAME, channel, chip).strip()
 
     if release_build == True:
-        print("Previous version:", curl_version)
-        print("Enter new version:")
-        version = input()
+        version = env["ENV"]["VERSION"]
 
     print(f"Version: {version}")
     print(f"Build Time: {buildTime}")
     print(f"Chip: {chip}")
-    print(f"Channel: {channel}")
-    print(f"OTA Version: {curl_version}")
-
-    if version == curl_version:
-        print("Version is up to date")
-        env.Exit(0)
 
     versionString = """
 #ifndef __VERSION_H__
@@ -71,16 +30,14 @@ def generate_version():
 
 #define FIRMWARE_VERSION "{version}"
 #define BUILD_TIME "{buildTime}"
-#define FIRMWARE_TYPE "{otaProjName}"
+#define FIRMWARE_TYPE "{firmwareType}"
 #define CHIP "{chip}"
 
 #endif
-""".format(version = version, buildTime = buildTime, chip = chip, otaProjName = OTA_PROJ_NAME)
+""".format(version = version, buildTime = buildTime, chip = chip, firmwareType = "STATION" )
 
     with open(versionPath, "w") as file:
         file.write(versionString)
-    with open(".versum", "w") as file:
-        file.write(filesHash.strip())
 
 env.AddPostAction("buildprog", after_build)
 generate_version()
