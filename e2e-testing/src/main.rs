edbb05d8ee598c81f2bd908670fc26a6701d1026
filipton <filipton12@gmail.com>
2024-04-27 16:23:46 +0200

    feat(e2e): state

--- e2e-testing/src/main.rs
+++ e2e-testing/src/main.rs
@@ -1,12 +1,26 @@
 use anyhow::Result;
-use std::path::Path;
-use structs::{CompetitionStatusResp, UnixRequest, UnixResponse, UnixResponseData};
+use std::{collections::HashMap, path::Path};
+use structs::{CompetitionStatusResp, Room, UnixRequest, UnixResponse, UnixResponseData};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{UnixListener, UnixStream},
 };
 
+use crate::structs::UnixRequestData;
+
 mod structs;
+
+pub struct State {
+    pub devices: Vec<u32>,
+    pub cards: HashMap<String, CompetitorInfo>,
+}
+
+pub struct CompetitorInfo {
+    pub registrant_id: i64,
+    pub name: String,
+    pub wca_id: String,
+    pub can_compete: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -15,17 +29,31 @@ async fn main() -> Result<()> {
     _ = tokio::fs::create_dir_all(socket_dir).await;
     _ = tokio::fs::remove_file(&socket_path).await;
 
+    let mut state = State {
+        devices: vec![],
+        cards: HashMap::new(),
+    };
+    state.cards.insert(
+        "3004425529".to_string(),
+        CompetitorInfo {
+            name: "Filip Sciurka".to_string(),
+            registrant_id: state.cards.len() as i64,
+            wca_id: "FILSCI01".to_string(),
+            can_compete: true,
+        },
+    );
+
     let mut device_store: Vec<u32> = vec![];
 
     let listener = UnixListener::bind(socket_path)?;
     loop {
         let (mut stream, _) = listener.accept().await?;
-        _ = handle_stream(&mut stream, &mut device_store).await;
+        _ = handle_stream(&mut stream, &mut state).await;
     }
 }
 
-async fn handle_stream(stream: &mut UnixStream, device_store: &mut Vec<u32>) -> Result<()> {
-    send_status_resp(stream, &device_store).await?;
+async fn handle_stream(stream: &mut UnixStream, state: &mut State) -> Result<()> {
+    send_status_resp(stream, &state.devices).await?;
 
     loop {
         let bytes = read_until_null(stream).await?;
@@ -33,11 +61,33 @@ async fn handle_stream(stream: &mut UnixStream, device_store: &mut Vec<u32>) -> 
 
         match packet.data {
             structs::UnixRequestData::RequestToConnectDevice { esp_id, .. } => {
-                device_store.push(esp_id);
-                send_status_resp(stream, &device_store).await?;
-                send_resp(stream, UnixResponseData::Empty, packet.tag).await?;
+                state.devices.push(esp_id);
+                send_status_resp(stream, &state.devices).await?;
+                send_resp(stream, UnixResponseData::Empty, packet.tag, false).await?;
             }
-            _ => {}
+            structs::UnixRequestData::PersonInfo { ref card_id } => {
+                let competitor = state.cards.get(card_id);
+                let resp = match competitor {
+                    Some(competitor) => UnixResponseData::PersonInfoResp {
+                        id: card_id.to_string(),
+                        registrant_id: Some(competitor.registrant_id),
+                        name: competitor.name.to_string(),
+                        wca_id: Some(competitor.wca_id.to_string()),
+                        country_iso2: Some("PL".to_string()),
+                        gender: "Male".to_string(),
+                        can_compete: competitor.can_compete,
+                    },
+                    None => UnixResponseData::Error {
+                        message: "Competitor not found".to_string(),
+                        should_reset_time: false,
+                    },
+                };
+
+                send_resp(stream, resp, packet.tag, competitor.is_none()).await?;
+            }
+            _ => {
+                send_resp(stream, UnixResponseData::Empty, packet.tag, false).await?;
+            }
         }
 
         println!("{packet:?}");
@@ -48,10 +98,11 @@ async fn send_resp(
     stream: &mut UnixStream,
     data: UnixResponseData,
     tag: Option<u32>,
+    error: bool,
 ) -> Result<()> {
     let packet = UnixResponse {
         tag,
-        error: None,
+        error: Some(error),
         data: Some(data),
     };
 
@@ -68,7 +119,12 @@ async fn send_status_resp(stream: &mut UnixStream, device_store: &Vec<u32>) -> R
         data: Some(UnixResponseData::ServerStatus(CompetitionStatusResp {
             should_update: true,
             devices: device_store.to_vec(),
-            rooms: vec![],
+            rooms: vec![Room {
+                id: "dsa".to_string(),
+                name: "room 1".to_string(),
+                devices: device_store.to_vec(),
+                use_inspection: true,
+            }],
         })),
     };
 
