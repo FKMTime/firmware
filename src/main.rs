0df426a14e16d3d9672249e931d5cb2812d02cf0
filipton <filipton12@gmail.com>
2024-09-25 23:00:06 +0200

    feat: init rust project

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+};
+
+extern crate alloc;
+use core::mem::MaybeUninit;
+
+#[global_allocator]
+static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
+
+fn init_heap() {
+    const HEAP_SIZE: usize = 32 * 1024;
+    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
+
+    unsafe {
+        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = SystemControl::new(peripherals.SYSTEM);
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let delay = Delay::new(&clocks);
+    init_heap();
+
+    esp_println::logger::init_logger_from_env();
+
+    let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0, &clocks);
+    let _init = esp_wifi::initialize(
+        esp_wifi::EspWifiInitFor::Wifi,
+        timg0.timer0,
+        esp_hal::rng::Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+        &clocks,
+    )
+    .unwrap();
+
+    loop {
+        log::info!("Hello world!");
+        delay.delay(500.millis());
+    }
+}
