a54074ed77ba6a67f6e2dc6145d62a0784fe8f78
filipton <filipton12@gmail.com>
2024-09-27 16:02:01 +0200

    feat: async (embassy) + esp-hal-wifimanager (ap not working for now)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 #![no_std]
 #![no_main]
 
+use core::mem::MaybeUninit;
+use embassy_executor::Spawner;
+use embassy_time::Timer;
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+    clock::{ClockControl, Clocks},
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemControl,
+    timer::timg::TimerGroup,
 };
 
 extern crate alloc;
-use core::mem::MaybeUninit;
 
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
 fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
+    const HEAP_SIZE: usize = 20 * 1024;
     static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
 
     unsafe {
@@ -21,29 +27,57 @@ fn init_heap() {
     }
 }
 
-#[entry]
-fn main() -> ! {
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
+
+#[main]
+async fn main(spawner: Spawner) {
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
+    let clocks =
+        ClockControl::configure(system.clock_control, esp_hal::clock::CpuClock::Clock80MHz)
+            .freeze();
 
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let delay = Delay::new(&clocks);
+    let clocks = &*mk_static!(Clocks<'static>, clocks);
     init_heap();
 
     esp_println::logger::init_logger_from_env();
 
+    let timg1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    esp_hal_embassy::init(&clocks, timg1.timer0);
+
+    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
+
+    let mut wm_settings = esp_hal_wifimanager::WmSettings::default();
+    wm_settings.ssid_generator = |efuse| {
+        let mut generated_name = heapless::String::<32>::new();
+        _ = core::fmt::write(&mut generated_name, format_args!("TEST-{:X}", efuse));
+        generated_name
+    };
+
     let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0, &clocks);
-    let _init = esp_wifi::initialize(
-        esp_wifi::EspWifiInitFor::Wifi,
+    let wifi_res = esp_hal_wifimanager::init_wm(
+        wm_settings,
         timg0.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
+        rng.clone(),
         peripherals.RADIO_CLK,
         &clocks,
+        peripherals.WIFI,
+        peripherals.BT,
+        &spawner,
     )
-    .unwrap();
+    .await;
+
+    log::info!("wifi_res: {wifi_res:?}");
 
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        log::info!("bump {}", esp_hal::time::current_time());
+        Timer::after_millis(15000).await;
     }
 }
