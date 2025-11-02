d47aa285d0ad7ee19c5cb6a79778e3f313260f72
filipton <filipton12@gmail.com>
2025-11-02 10:22:43 +0100

    feat: different battery curves

--- src/battery.rs
+++ src/battery.rs
@@ -1,26 +1,29 @@
 use crate::{
     consts::BATTERY_SEND_INTERVAL_MS, state::sleep_state, utils::rolling_average::RollingAverage,
 };
-use embassy_time::{Instant, Timer};
+use embassy_time::{Duration, Instant, Timer};
 use esp_hal::analog::adc::{Adc, AdcConfig, Attenuation};
 
-type AdcCal = esp_hal::analog::adc::AdcCalBasic<esp_hal::peripherals::ADC1<'static>>;
+type AdcCal = esp_hal::analog::adc::AdcCalCurve<esp_hal::peripherals::ADC1<'static>>;
 
-const BAT_MIN: f64 = 3200.0;
-const BAT_MAX: f64 = 4200.0;
-const BATTERY_CURVE: [(f64, u8); 11] = [
+const BATTERY_CURVE: [(f64, u8); 14] = [
     (3200.0, 0),
     (3250.0, 5),
     (3300.0, 10),
     (3350.0, 20),
     (3400.0, 30),
+    (3450.0, 35),
     (3500.0, 40),
+    (3550.0, 45),
     (3600.0, 50),
     (3700.0, 60),
     (3800.0, 70),
     (3900.0, 80),
-    (4200.0, 100),
+    (4000.0, 90),
+    (4100.0, 100),
 ];
+const BAT_MIN: f64 = BATTERY_CURVE[0].0;
+const BAT_MAX: f64 = BATTERY_CURVE[BATTERY_CURVE.len() - 1].0;
 
 #[embassy_executor::task]
 pub async fn battery_read_task(
@@ -33,7 +36,7 @@ pub async fn battery_read_task(
     let mut adc_pin = adc_config.enable_pin_with_cal::<_, AdcCal>(adc_pin, Attenuation::_11dB);
     let mut adc = Adc::new(adc, adc_config).into_async();
 
-    let mut battery_start = Instant::now();
+    let mut battery_start = Instant::now().saturating_add(Duration::from_millis(300));
 
     let base_freq = 2.0;
     let sample_freq = 1000.0;
@@ -43,21 +46,13 @@ pub async fn battery_read_task(
     let mut lcd_sent = false;
 
     loop {
-        Timer::after_millis(100).await;
+        Timer::after_millis(10).await;
         if sleep_state() {
             Timer::after_millis(500).await;
             continue;
         }
 
         let read = adc.read_oneshot(&mut adc_pin).await;
-        if !lcd_sent {
-            state
-                .show_battery
-                .signal(bat_percentage(calculate(read as f64)));
-
-            lcd_sent = true;
-        }
-
         let read = smoother.tick(read as f32);
         avg.push(read);
 
@@ -67,7 +62,16 @@ pub async fn battery_read_task(
             state.current_bat_read = Some(read);
         }
 
-        if (Instant::now() - battery_start).as_millis() < BATTERY_SEND_INTERVAL_MS {
+        let now = Instant::now();
+        if !lcd_sent && battery_start <= now {
+            state
+                .show_battery
+                .signal(bat_percentage(calculate(read as f64)));
+
+            lcd_sent = true;
+        }
+
+        if battery_start > now || (now - battery_start).as_millis() < BATTERY_SEND_INTERVAL_MS {
             continue;
         }
 
@@ -123,5 +127,5 @@ fn bat_percentage(mv: f64) -> u8 {
 }
 
 fn calculate(x: f64) -> f64 {
-    1.18323 * x + 276.754
+    1.69874 * x + 66.6103
 }
